8bafde2017036ebfadac59ca9233353078fa981a
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2026-01-12 22:17:20 +0900

    Initial Commit

---
+++ GNCurrencyConvertor/GNCurrencyConvertorApp.swift
@@ -0,0 +1,17 @@
+//
+//  GNCurrencyConvertorApp.swift
+//  GNCurrencyConvertor
+//
+//  Created by joe on 1/12/26.
+//
+
+import SwiftUI
+
+@main
+struct GNCurrencyConvertorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
