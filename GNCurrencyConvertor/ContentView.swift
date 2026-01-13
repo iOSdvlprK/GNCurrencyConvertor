7529eb22b8839d46c53c6fbf82a9d3f5e23b0346
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2026-01-13 22:54:11 +0900

    add currency, flag assets and basic ui
    - added image assets and Contents.json files for various countries and currencies to the Assets.xcassets directory
    - implemented main basic user interface

--- GNCurrencyConvertor/ContentView.swift
+++ GNCurrencyConvertor/ContentView.swift
@@ -8,14 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var amount = ""
+    @State private var conversion = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(alignment: .leading) {
+            Text("Amount")
+                .font(.system(size: 15))
+            TextField("", text: $amount)
+                .font(.system(size: 18, weight: .semibold))
+                .padding()
+                .overlay {
+                    RoundedRectangle(cornerRadius: 5)
+                        .fill(.clear)
+                        .stroke(Color.gray, lineWidth: 1)
+                }
+            HStack {
+                Spacer()
+                Image(systemName: "arrow.up.arrow.down")
+                    .font(.system(size: 20, weight: .bold))
+                    .padding(.vertical)
+                Spacer()
+            }
+            Text("Converted to")
+                .font(.system(size: 15))
+            TextField("", text: $conversion)
+                .font(.system(size: 18, weight: .semibold))
+                .padding()
+                .overlay {
+                    RoundedRectangle(cornerRadius: 5)
+                        .fill(.clear)
+                        .stroke(Color.gray, lineWidth: 1)
+                }
+            HStack {
+                Spacer()
+                Text("1.00000 USD = 2.00000 EUR")
+                    .font(.system(size: 18, weight: .semibold))
+                    .padding(.top, 25)
+                Spacer()
+            }
         }
-        .padding()
+        .padding(.horizontal)
     }
 }
 
